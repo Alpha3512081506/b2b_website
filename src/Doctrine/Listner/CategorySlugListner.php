d470cbf9a8a70bfc22ca3772d8528caa5e36d27f
Alpha Barry <barrymamadoualpha2@gmail.com>
2021-01-25 20:51:08 +0100

    deplyement

---
+++ src/Doctrine/Listner/CategorySlugListner.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Doctrine\Listner;
+
+use App\Entity\Categoria;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategorySlugListner
+{
+    private $slugger;
+    public function __construct(SluggerInterface $sluggerInterface)
+    {
+        $this->slugger = $sluggerInterface;
+    }
+    public function prePersist(Categoria $entity, LifecycleEventArgs $lifecycleEventArgs)
+    {
+
+        if (empty($entity->getSlug())) {
+            $entity->setSlug(strtolower($this->slugger->slug($entity->getNomeCategoria())));
+        }
+    }
+}
---
+++ src/Doctrine/Listner/ProductSlugListner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Doctrine\Listner;
+
+use App\Entity\Prodotto;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProductSlugListner
+{
+    private $slug;
+    public function __construct(SluggerInterface $slug)
+    {
+        $this->slug = $slug;
+    }
+
+    public function prePersist(Prodotto $entity, LifecycleEventArgs $lifecycleEventArgs): void
+    {
+
+        if (empty($entity->getSlug())) {
+            $entity->setSlug($this->slug->slug(strtolower($entity->getNomeStile())));
+        }
+    }
+}
