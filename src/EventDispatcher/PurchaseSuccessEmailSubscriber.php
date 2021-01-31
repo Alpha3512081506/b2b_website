1b08c9f30aecd3cf1068ae7a4623b2d50a761692
Alpha Barry <barrymamadoualpha2@gmail.com>
2021-01-31 13:17:41 +0100

    creation de footer

--- src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
+++ src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
@@ -2,11 +2,24 @@
 
 namespace App\EventDispatcher;
 
+use App\Entity\User;
 use App\Event\PurchaseSuccessEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PurchaseSuccessEmailSubscriber  implements EventSubscriberInterface
 {
+    private $mailer;
+    private $security;
+    public  function __construct(MailerInterface $mailerInterface, Security $security)
+    {
+        $this->mailer = $mailerInterface;
+        $this->security = $security;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -15,6 +28,19 @@ class PurchaseSuccessEmailSubscriber  implements EventSubscriberInterface
     }
     public function sendSuccessEmail(PurchaseSuccessEvent $purchaseSuccessEvent)
     {
-        // dd($purchaseSuccessEvent);
+        /**@var User */
+        $currentUser = $this->security->getUser();
+        $purchase = $purchaseSuccessEvent->getPurchase();
+        $email = new TemplatedEmail();
+        $email->to(
+            new Address($currentUser->getEmail()),
+            $currentUser->getFullName()
+        )->from("mamadoualphabarry780@gmail.com
+         ")
+            ->subject("Bravo il suo ordine {$purchase->getId()} è stato confermato con successo")
+            ->htmlTemplate("emails/purchase_success.html.twig")->context(
+                ['purchase' => $purchase, 'user' => $currentUser]
+            );
+        $this->mailer->send($email);
     }
 }
